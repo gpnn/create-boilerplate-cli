e02aaf9958ad2649d66b761f328e330d49e1c95e
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-03-14 15:12:22 -0400

    feat:  add stylelint and eslint

--- init_boilerplate.rb
+++ init_boilerplate.rb
@@ -10,8 +10,10 @@ TS_LINT = 'tslint.json'
 TS_CONFIG = 'tsconfig.json'
 LINT_STAGED = '.lintstagedrc'
 HUSKY = '.huskyrc'
+ES_LINT = '.eslintrc.json'
+STYLE_LINT = '.stylelintrc.json'
 
-CONFIGS = []
+CONFIGS = [].freeze
 
 CONFIGS << PACKAGE
 CONFIGS << PRETTIER
@@ -20,11 +22,44 @@ CONFIGS << TS_LINT
 CONFIGS << TS_CONFIG
 CONFIGS << LINT_STAGED
 CONFIGS << HUSKY
+CONFIGS << ES_LINT
+CONFIGS << STYLE_LINT
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
 
 def display_header
-  puts "\n========================================"
-  puts "\tInitialize Boilerplate\n"
-  puts "========================================\n"
+  puts "\n========================================".colorize(blue)
+  puts "\tInitialize Boilerplate\n".colorize(pink)
+  puts "========================================\n".colorize(blue)
   puts "\nThe current working directory is:\n"
   puts Dir.pwd
 end
@@ -36,41 +71,44 @@ def display_menu
   puts "3. Add GitHub templates\n"
   puts "4. Exit\n\n"
   puts 'Your choice: '
-  input = gets.chomp.strip
+  input = STDIN.gets.chomp.strip
   process_option(input.to_i)
-end
-
-def create_react_project
-  check_npm
-  check_already_init
-  system('npx create-react-app .')
 end
 
 def check_already_init
   if File.file?(PACKAGE)
-    puts "#{PACKAGE} exists and will be used"
+    puts "#{PACKAGE} exists and will be used".colorize(green)
   else
-    puts "#{PACKAGE} does not exist"
+    puts "#{PACKAGE} does not exist".colorize(green)
     system('npm init -y')
+    package_json = File.read(PACKAGE)
+    package_hash = JSON.parse(package_json)
+    package_hash['description'] = 'TODO'
+    package_hash['repository'] = 'TODO' unless package_hash.key?('repository')
+    File.open(PACKAGE, 'w+') do |file|
+      file.write(JSON.pretty_generate(package_hash))
+      file.close
+    end
   end
 end
 
 def check_npm
   return unless system('npm --version').nil?
 
-  puts 'Error: npm is not installed'
+  puts 'Error: npm is not installed'.colorize(red)
   exit(1)
 end
 
 def create_node_directory
+  puts 'Creating src directory...'.colorize(green)
   path = './src/index.ts'
   dir = File.dirname(path)
   FileUtils.mkdir_p(dir) unless File.directory?(dir)
-  File.new(path, 'w')
+  File.new(path, 'w+')
 end
 
 def configure_nodemon
-  puts 'Configuring nodemon...'
+  puts 'Configuring nodemon...'.colorize(green)
   if File.exist?(NODEMON)
     return "#{NODEMON} already exists and will not be configured"
   end
@@ -81,80 +119,127 @@ def configure_nodemon
       ignore: [],
       exec: 'ts-node ./src/index.ts'
   }
-  File.open(NODEMON, 'w') do |file|
+  File.open(NODEMON, 'w+') do |file|
+    file.truncate(0)
     file.write(JSON.pretty_generate(hash))
+    file.close
   end
 end
 
 def configure_ts_lint
-  puts 'Configuring tslint...'
-  # system('npx tslint --init')
+  puts 'Configuring tslint...'.colorize(green)
+  system('npx tslint --init')
   ts_lint = File.read(TS_LINT)
   ts_lint_hash = JSON.parse(ts_lint)
   if ts_lint_hash.key?('rules') && ts_lint_hash['rules'].is_a?(Hash)
     ts_lint_hash['rules']['no-console'] = false
+    ts_lint_hash['rules']['prettier'] = true
   end
-  File.delete(TS_LINT)
-  File.open(TS_LINT, 'w') do |file|
+  ts_lint_hash['extends'] << 'tslint-config-prettier'
+  ts_lint_hash['extends'] << 'tslint-plugin-prettier'
+  File.open(TS_LINT, 'w+') do |file|
     file.write(JSON.pretty_generate(ts_lint_hash))
+    file.close
   end
 end
 
 def update_node_package_json
+  puts "Updating #{PACKAGE} scripts...".colorize(green)
   package_json = File.read(PACKAGE)
   package_hash = JSON.parse(package_json)
   package_hash['scripts']['start:dev'] = 'nodemon'
   package_hash['scripts']['build'] = 'rimraf ./build && tsc'
   package_hash['scripts']['start'] = 'npm run build && node build/index.js'
-  File.delete(PACKAGE)
-  File.open(PACKAGE, 'w') do |file|
+  File.open(PACKAGE, 'w+') do |file|
     file.write(JSON.pretty_generate(package_hash))
+    file.close
   end
 end
 
 def configure_prettier
-  puts 'Installing and configuring prettier...'
-  system('npm install --save-dev prettier')
+  puts 'Installing and configuring prettier...'.colorize(green)
+  system('npm install --save-dev prettier tslint-config-prettier tslint-plugin-prettier')
   if File.exist?(PRETTIER)
     return "#{PRETTIER} already exists and will not be configured"
   end
-
   hash = {
       trailingComma: 'es5',
       arrowParens: 'always',
-      singleQuote: true
+      singleQuote: true,
+      printWidth: 120
   }
-  File.open(PRETTIER, 'w') do |file|
+  File.open(PRETTIER, 'w+') do |file|
     file.write(JSON.pretty_generate(hash))
+    file.close
   end
 end
 
 def configure_typescript
   create_node_directory
-  puts 'Installing and configuring typescript...'
-  # system('npm install --save-dev typescript tslint @types/node \
-  #   ts-node nodemon rimraf @types/express')
-  # system('npm install express')
-  configure_nodemon
-  # system('npx tsc --init --rootDir src --outDir build --moduleResolution "node" \
-  #   --esModuleInterop --resolveJsonModule --lib esnext --sourceMap \
-  #   --module commonjs --allowJs true --noImplicitAny true --target "esnext"')
-  # configure_ts_lint
-  update_node_package_json
-  configure_prettier
+  puts 'Installing and configuring typescript...'.colorize(green)
+  system('npm install --save-dev typescript tslint @types/node ts-node nodemon rimraf @types/express')
+  system('npm install express')
+  system('npx tsc --init --rootDir src --outDir build --moduleResolution "node" --esModuleInterop --resolveJsonModule --lib esnext --sourceMap --module commonjs --allowJs true --noImplicitAny true --target "esnext"')
+end
+
+def configure_eslint(react)
+  puts 'Installing and configuring ESLint...'.colorize(green)
+  system('npm install --save-dev eslint eslint-plugin-prettier eslint-config-prettier')
+  system('npx eslint --init')
+  es_lint = File.read(ES_LINT)
+  es_lint_hash = JSON.parse(es_lint)
+  es_lint_hash['plugins'] << 'prettier'
+  es_lint_hash['rules']['prettier/prettier'] = 'error'
+  es_lint_hash['rules']['no-console'] = 1
+  es_lint_hash['extends'] << 'prettier'
+  es_lint_hash['extends'] << 'plugin:prettier/recommended'
+  es_lint_hash['extends'] << 'prettier/react' if react
+  File.open(ES_LINT, 'w+') do |file|
+    file.write(JSON.pretty_generate(es_lint_hash))
+    file.close
+  end
 end
 
 def install_node_dependencies
-  puts 'Installing dev dependencies...'
-  # system('npm install --save-dev eslint husky lint-staged commitlint stylelint')
-  configure_typescript
+  puts 'Installing dev dependencies...'.colorize(green)
+  system('npm install --save-dev husky lint-staged commitlint')
+end
+
+def configure_stylelint
+  puts 'Installing and configuring stylelint...'.colorize(green)
+  system('npm install --save-dev stylelint stylelint-config-standard stylelint-config-sass-guidelines stylelint-prettier stylelint-config-prettier')
+  hash = {
+      extends: %w[stylelint-config-standard stylelint-config-sass-guidelines stylelint-prettier/recommended],
+      plugins: ['stylelint-prettier'],
+      rules: {
+          'prettier/prettier': true
+      }
+  }
+  File.open(STYLE_LINT, 'w+') do |file|
+    file.write(JSON.pretty_generate(hash))
+    file.close
+  end
+
 end
 
 def create_node_project
-  puts 'Creating node.js project'
+  puts 'Creating node.js project'.colorize(green)
   check_npm
   check_already_init
   install_node_dependencies
+  configure_typescript
+  configure_nodemon
+  configure_ts_lint
+  update_node_package_json
+  configure_prettier
+  configure_eslint(false)
+end
+
+def create_react_project
+  puts 'Creating react.js project'
+  check_npm
+  check_already_init
+  system('npx create-react-app .')
 end
 
 def add_templates
@@ -182,7 +267,7 @@ def process_option(choice)
 end
 
 def clean_directory
-  puts "\nCleaning directory..."
+  puts "\nCleaning directory...".colorize(light_blue)
   FileUtils.rm_rf 'src' if File.directory?('src')
   CONFIGS.each do |config|
     File.delete(config) if File.exist?(config)
@@ -192,11 +277,8 @@ end
 
 def main
   display_header
-  if ARGV[0] == 'clean'
-    clean_directory
-  else
-    display_menu
-  end
+  clean_directory if ARGV[0] == 'clean'
+  display_menu
 end
 
 main
