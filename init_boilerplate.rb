6fb23e0440889d5daf8d1de50dc4d6c20b6439f2
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-03-14 17:40:49 -0400

    feat:  remove tslint, add optional typescript, support for react.js

--- init_boilerplate.rb
+++ init_boilerplate.rb
@@ -6,7 +6,6 @@ require 'json'
 PACKAGE = 'package.json'
 PRETTIER = '.prettierrc'
 NODEMON = 'nodemon.json'
-TS_LINT = 'tslint.json'
 TS_CONFIG = 'tsconfig.json'
 LINT_STAGED = '.lintstagedrc'
 HUSKY = '.huskyrc'
@@ -19,7 +18,6 @@ CONFIGS = []
 CONFIGS << PACKAGE
 CONFIGS << PRETTIER
 CONFIGS << NODEMON
-CONFIGS << TS_LINT
 CONFIGS << TS_CONFIG
 CONFIGS << LINT_STAGED
 CONFIGS << HUSKY
@@ -101,9 +99,13 @@ def check_npm
   exit(1)
 end
 
-def create_node_directory
+def create_node_directory(typescript)
   puts 'Creating src directory...'.green
-  path = './src/index.ts'
+  path = if typescript
+           './src/index.ts'
+         else
+           './src/index.js'
+         end
   dir = File.dirname(path)
   FileUtils.mkdir_p(dir) unless File.directory?(dir)
   File.new(path, 'w+')
@@ -111,36 +113,20 @@ end
 
 def configure_nodemon
   puts 'Configuring nodemon...'.green
+  system('npm install --save-dev nodemon')
   if File.exist?(NODEMON)
     return "#{NODEMON} already exists and will not be configured"
   end
 
   hash = {
-    watch: ['src'],
-    ext: '.ts,.js',
-    ignore: [],
-    exec: 'ts-node ./src/index.ts'
+      watch: ['src'],
+      ext: '.ts,.js',
+      ignore: [],
+      exec: 'ts-node ./src/index.ts'
   }
   File.open(NODEMON, 'w+') do |file|
     file.truncate(0)
     file.write(JSON.pretty_generate(hash))
-    file.close
-  end
-end
-
-def configure_ts_lint
-  puts 'Configuring tslint...'.green
-  system('npx tslint --init')
-  ts_lint = File.read(TS_LINT)
-  ts_lint_hash = JSON.parse(ts_lint)
-  if ts_lint_hash.key?('rules') && ts_lint_hash['rules'].is_a?(Hash)
-    ts_lint_hash['rules']['no-console'] = false
-    ts_lint_hash['rules']['prettier'] = true
-  end
-  ts_lint_hash['extends'] << 'tslint-config-prettier'
-  ts_lint_hash['extends'] << 'tslint-plugin-prettier'
-  File.open(TS_LINT, 'w+') do |file|
-    file.write(JSON.pretty_generate(ts_lint_hash))
     file.close
   end
 end
@@ -160,16 +146,16 @@ end
 
 def configure_prettier
   puts 'Installing and configuring prettier...'.green
-  system('npm install --save-dev prettier tslint-config-prettier tslint-plugin-prettier')
+  system('npm install --save-dev prettier')
   if File.exist?(PRETTIER)
     return "#{PRETTIER} already exists and will not be configured"
   end
 
   hash = {
-    trailingComma: 'es5',
-    arrowParens: 'always',
-    singleQuote: true,
-    printWidth: 120
+      trailingComma: 'es5',
+      arrowParens: 'always',
+      singleQuote: true,
+      printWidth: 120
   }
   File.open(PRETTIER, 'w+') do |file|
     file.write(JSON.pretty_generate(hash))
@@ -177,11 +163,15 @@ def configure_prettier
   end
 end
 
-def configure_typescript
-  create_node_directory
+def configure_typescript(react)
   puts 'Installing and configuring typescript...'.green
-  system('npm install --save-dev typescript tslint @types/node ts-node nodemon rimraf @types/express')
-  system('npm install express')
+  system('npm install --save-dev typescript @types/node ts-node rimraf')
+  if react
+    system('npm install --save-dev @types/react @types/react-dom @types/jest')
+  else
+    system('npm install --save-dev @types/express')
+    system('npm install express')
+  end
   system('npx tsc --init --rootDir src --outDir build --moduleResolution "node" --esModuleInterop --resolveJsonModule --lib esnext --sourceMap --module commonjs --allowJs true --noImplicitAny true --target "esnext"')
 end
 
@@ -196,27 +186,26 @@ def configure_eslint(react)
   es_lint_hash['rules']['no-console'] = 1
   es_lint_hash['extends'] << 'prettier'
   es_lint_hash['extends'] << 'plugin:prettier/recommended'
-  es_lint_hash['extends'] << 'prettier/react' if react
+  if react
+    es_lint_hash['plugins'] << 'react'
+    es_lint_hash['extends'] << 'plugin:react/recommended'
+    es_lint_hash['extends'] << 'prettier/react'
+  end
   File.open(ES_LINT, 'w+') do |file|
     file.write(JSON.pretty_generate(es_lint_hash))
     file.close
   end
 end
 
-def install_node_dependencies
-  puts 'Installing dev dependencies...'.green
-  system('npm install --save-dev husky lint-staged')
-end
-
 def configure_stylelint
   puts 'Installing and configuring stylelint...'.green
   system('npm install --save-dev stylelint stylelint-config-standard stylelint-config-sass-guidelines stylelint-prettier stylelint-config-prettier')
   hash = {
-    extends: %w[stylelint-config-standard stylelint-config-sass-guidelines stylelint-prettier/recommended],
-    plugins: ['stylelint-prettier'],
-    rules: {
-      'prettier/prettier': true
-    }
+      extends: %w[stylelint-config-standard stylelint-config-sass-guidelines stylelint-prettier/recommended],
+      plugins: ['stylelint-prettier'],
+      rules: {
+          'prettier/prettier': true
+      }
   }
   File.open(STYLE_LINT, 'w+') do |file|
     file.write(JSON.pretty_generate(hash))
@@ -227,7 +216,7 @@ end
 def configure_commitlint
   puts 'Installing and configuring commitlint...'
   system('npm install --save-dev @commitlint/{config-conventional,cli} stylefmt')
-  hash = { extends: ['@commitlint/config-conventional'] }
+  hash = {extends: ['@commitlint/config-conventional']}
   File.open(COMMIT_LINT, 'w+') do |file|
     file.write(JSON.pretty_generate(hash))
     file.close
@@ -242,19 +231,24 @@ def configure_husky
   package_hash['husky']['hooks']['commit-msg'] = 'commitlint -E HUSKY_GIT_PARAMS'
   git_add = 'git add'
   package_hash['lint-staged'] = {
-    '*.+(js|jsx|ts|tsx)': [
-      'eslint --cache --fix',
-      git_add
-    ],
-    '*.+(json|css|md|html|ts|tsx|jsx)': [
-      'prettier --write',
-      git_add
-    ],
-    '*.+(css|less|sass)': [
-      'stylefmt',
-      'stylelint --fix',
-      git_add
-    ]
+      '*.+(js|jsx|ts|tsx)': [
+          'eslint --cache --fix',
+          git_add
+      ],
+      '*.+(json|css|md|html|ts|tsx|jsx)': [
+          'prettier --write',
+          git_add
+      ],
+      '*.+(css|less|sass)': [
+          'stylefmt',
+          'stylelint --fix',
+          git_add
+      ],
+      '*.+(scss)': [
+          'stylefmt',
+          'stylelint --fix --syntax=scss',
+          git_add
+      ]
   }
   File.open(PACKAGE, 'w+') do |file|
     file.write(JSON.pretty_generate(package_hash))
@@ -266,10 +260,12 @@ def create_node_project
   puts 'Creating node.js project'.green
   check_npm
   check_already_init
-  install_node_dependencies
-  configure_typescript
+  puts "Does your project use TypeScript? (y/n)\n"
+  input = STDIN.gets.strip.chomp
+  typescript = input == 'y'
+  configure_typescript(false) if typescript
+  create_node_directory(typescript)
   configure_nodemon
-  configure_ts_lint
   update_node_package_json
   configure_prettier
   configure_eslint(false)
@@ -281,7 +277,19 @@ def create_react_project
   puts 'Creating react.js project'
   check_npm
   check_already_init
-  system('npx create-react-app .')
+  puts "Does your project use TypeScript? (y/n)\n"
+  input = STDIN.gets.strip.chomp
+  typescript = input == 'y'
+  if typescript
+    configure_typescript(true)
+    system('npx create-react-app . --template typescript')
+  else
+    system('npx create-react-app .')
+  end
+  configure_prettier
+  configure_eslint(true)
+  configure_commitlint
+  configure_husky
 end
 
 def add_templates
